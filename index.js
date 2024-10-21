c4ca4df669fe937b249d1ebffc00c711e929b7c0
KateZone <134741223+KateZone@users.noreply.github.com>
2024-10-20 23:50:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-var name = prompt("What is your name: ")
-alert(name);
+alert("Hi Kate, I'm the external file.")
+document.getElementsByTagName("li")[2].style.color="purple";
+document.getElementById("sticker").innerHTML = "yellow";
+document.getElementById("sticker").style.color = "yellow";
+document.querySelector("li").style.color="black";
+document.querySelector("body").style.backgroundColor="lightblue";
+
+document.getElementById("sticker").classList.add("huge");
+
+document.querySelector("a");
+
+
